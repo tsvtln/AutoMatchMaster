4a8c7422084c02fb0f09f2e1b3bf1928183e8d70
TSVTLN <112159858+tsvtln@users.noreply.github.com>
2024-06-18 11:30:14 +0300

    pu

--- kc.py
+++ kc.py
@@ -117,14 +117,13 @@ matches_penta = {
 
 matrix = [
     ['G', 'R', 'Y', 'G', 'R', 'P', 'B'],
-    ['Y', 'B', 'Y', 'G', 'P', 'O', 'Y'],
-    ['G', 'P', 'B', 'B', 'R', 'B', 'B'],
+    ['Y', 'B', 'Y', 'G', 'B', 'B', 'Y'],
+    ['G', 'P', 'B', 'B', 'R', 'R', 'B'],
     ['G', 'Y', 'Y', 'O', 'B', 'P', 'R'],
     ['B', 'O', 'R', 'B', 'B', 'P', 'R'],
     ['R', 'G', 'O', 'Y', 'G', 'Y', 'P'],
     ['Y', 'O', 'G', 'B', 'G', 'P', 'R']
 ]
-
 
 for row in range(0, 7):
     if quad or penta:
@@ -137,44 +136,60 @@ for row in range(0, 7):
 
         # CHECK MOVE RIGHT
 
-        # Check right matches (3p)
-        if col != 6 and col != 5 and col != 4:
-            if matrix[row][col + 2] == color and matrix[row][col + 3] == color:
+        # 3ple - 2 right
+        if col not in [4, 5, 6]:
+            if (matrix[row][col + 2] == color
+                    and matrix[row][col + 3] == color):
+                matches_triple['match_right'][color_name].append((row, col))
+        # 3ple - above + bellow
+        if row not in [0, 6] and col != 6:
+            if (matrix[row - 1][col + 1] == color
+                    and matrix[row + 1][col + 1] == color):
+                matches_triple['match_right'][color_name].append((row, col))
+        # 3ple - 2 above
+        if row not in [0, 1, 2] and col != 6:
+            if (matrix[row - 1][col + 1] == color and
+                    matrix[row - 2][col + 1] == color):
+                matches_triple['match_right'][color_name].append((row, col))
+        # 3ple - 2 bellow
+        if row not in [4, 5, 6] and col != 6:
+            if (matrix[row+1][col+1] == color and
+                    matrix[row+2][col+1] == color):
                 matches_triple['match_right'][color_name].append((row, col))
 
-        # Check up and down matches (3p)
-        if row != 0 and row != 6 and col != 6:
-            if matrix[row - 1][col + 1] == color and matrix[row + 1][col + 1] == color:
-                matches_triple['match_right'][color_name].append((row, col))
-        if row != 6 and row != 5 and row != 4 and col != 6:
-            if matrix[row + 1][col + 1] == color and matrix[row + 2][col + 1] == color:
-                matches_triple['match_right'][color_name].append((row, col))
-
-
-        # Check up and down matches (4d)
-        if row != 0 and row != 6 and col != 6:
+        # quad - 1 above, 2 bellow
+        if row not in [0, 5, 6] and col != 6:
+            if (matrix[row - 1][col +1] == color and
+                    matrix[row + 1][col + 1] == color and
+                    matrix[row + 2][col + 1] == color):
+                quad = True
+                matches_quad['match_right'][color_name].append((row, col))
+        # quad - 1 bellow, 2 above
+        if row not in [6, 0, 1] and col != 6:
             if (matrix[row + 1][col + 1] == color and
                     matrix[row - 1][col + 1] == color and
                     matrix[row - 2][col + 1] == color):
+                quad = True
                 matches_quad['match_right'][color_name].append((row, col))
 
+        # penta - 2 above, 2 bellow
+        if row not in [0, 1, 5, 6] and col != 6:
+            if (matrix[row - 1][col + 1] == color and
+                    matrix[row - 2][col + 1] == color and
+                    matrix[row + 1][col + 1] == color and
+                    matrix[row + 2][col + 2] == color):
+                penta = True
+                matches_penta['match_right'][color_name].append((row, col))
 
 
-
-        # # Check left
-        # if col != 0:
-        #     pass
-        # # Check down
-        # if row != 6:
-        #     pass
-        # # Check up
-        # if col != 0:
-        #     pass
-
-
+print('triple')
 print(matches_triple)
 print('\n\n\n')
+print('quad')
 print(matches_quad)
+print('\n\n\n')
+print('penta')
+print(matches_penta)
 
 # print('3ple')
 # for direction, colors in matches_triple.items():
